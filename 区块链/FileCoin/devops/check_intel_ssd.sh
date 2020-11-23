a3b2f86daea38e3875edc06b74d01df1c118c2ef
oliver <flyboy8822@gmail.com>
2020-11-23 14:50:49 +0800

    del

--- 区块链/FileCoin/devops/check_intel_ssd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/usr/bin/intelmas show -a -smart -intelssd 0 | grep Action > /var/log/lotus/ssd/ssd_$(date +%Y%m%d).log
--- 区块链/FileCoin/devops/批量杀掉lotus进程xargs.md
+++
@@ -1,6 +0,0 @@
----
-title: 批量杀掉lotus进程xargs
-tags: 
----
-
-`ps -ef | grep lotus | awk '{print $2}' | xargs -L 1 kill`
